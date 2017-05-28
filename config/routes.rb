f6ac79776644d00dbd631c0ae8ee9b909e073a7b
nagisa-PC\nagisa <teradanagisa@gmail.com>
2017-05-28 15:33:46 +0900

    routing groups controller action定義

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
 
+  resources :groups, only: [:new, :create, :edit, :update]
+    resources :messages, only: [:create] do
+    end
+  end
+
 end
 
