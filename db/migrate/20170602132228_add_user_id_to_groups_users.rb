ec246b6fc8d983d157262e054776080033dd5e7e
nagisa-PC\nagisa <teradanagisa@gmail.com>
2017-06-04 14:39:39 +0900

    integer→referencesに。 messagesテーブルとgroups_usersテーブルカラム作成やりなおし。

--- db/migrate/20170602132228_add_user_id_to_groups_users.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToGroupsUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :groups_users, :user_id, :integer
-  end
-end
--- db/migrate/20170602132322_add_group_id_to_groups_users.rb
+++
@@ -1,5 +0,0 @@
-class AddGroupIdToGroupsUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :groups_users, :group_id, :integer
-  end
-end
