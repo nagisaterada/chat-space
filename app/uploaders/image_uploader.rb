f2fc03082fd1f4b318fe94251424ddcafb604cb6
nagisa-PC\nagisa <teradanagisa@gmail.com>
2017-06-29 16:34:58 +0900

    日付・時間のフォーマットを指定。

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,14 +1,20 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
+  include CarrierWave::MiniMagick
+
   storage :file
-
+  process convert: 'jpg'
+  # 保存するディレクトリ名
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
   # thumb バージョン(width 400px x height 200px)
   version :thumb do
-    process :resize_to_fit => [400, 200]
+  process :resize_to_fit => [400, 200]
+  end
+  # 許可する画像の拡張子
+  def extension_white_list
+  %W[jpg jpeg gif png]
   end
 
-end
+  end
