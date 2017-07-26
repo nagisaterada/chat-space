4e9622395ed39f8a3679c561046dabbf5d04f1eb
nagisa-PC\nagisa <teradanagisa@gmail.com>
2017-07-26 22:08:37 +0900

    「メッセージも画像もないと保存できない」テストコード　

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 describe Message do
   describe '#create' do
-    it "is invalid without a content" do
+    it "is invalid without a content" do #メッセージも画像もないと保存できない
+      message = Message.new(content: "", image: "", user_id: "000", group_id: "000")
+      message.valid?
+      expect(message.errors[:content]).to include("メッセージを入力してください")
     end
   end
 end
