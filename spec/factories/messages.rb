252d5caa678215859c076d4094e977e582b1b91d
nagisa-PC\nagisa <teradanagisa@gmail.com>
2017-07-29 20:17:31 +0900

    messages.rb作成

---
+++ spec/factories/messages.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :user do
+    content               "aaa"
+    image                 "aaa.jpg"
+    user_id               "000"
+    group_id              "000"
+  end
+
+end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
+
 describe Message do
   describe '#create' do
-    it "is invalid without a content" do #メッセージも画像もないと保存できない
+    it "is invalid without a content" do
       message = Message.new(content: "", image: "", user_id: "000", group_id: "000")
       message.valid?
       expect(message.errors[:content]).to include("メッセージを入力してください")
     end
-    it "is invalid without a user_id" do #user_idがないと保存できない
+    it "is invalid without a user_id" do
       message = Message.new(content: "aaa", image: "aaa.jpg", user_id: "", group_id: "000")
       message.valid?
       expect(message.errors[:user_id]).to include("不正な送信です")
     end
-    it "is invalid without a group_id" do #group_idがないと保存できない
+    it "is invalid without a group_id" do
       message = Message.new(content: "aaa", image: "aaa.jpg", user_id: "000", group_id: "")
       message.valid?
       expect(message.errors[:group_id]).to include("不正な送信です")
